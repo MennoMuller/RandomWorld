b8f7ca7d8743ba84aea7d1e898117ba29507d7e7
MennoMuller <menno.muller.nl@gmail.com>
2022-10-19 12:10:53 +0200

    Initial commit

---
+++ src/com/mennomuller/Main.java
@@ -0,0 +1,46 @@
+package com.mennomuller;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main extends Thread {
+    String myWord;
+
+    public Main() {
+        myWord = "world";
+    }
+
+    public Main(String str) {
+        myWord = str;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true){
+            Main main = new Main(input.nextLine().toLowerCase());
+            main.start();
+        }
+
+    }
+
+    @Override
+    public void run() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        long timeBefore = System.currentTimeMillis();
+        Future<Integer> future = executor.submit(new WordGenerator(myWord));
+        int attempts = 0;
+        try {
+            attempts = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        long timeAfter = System.currentTimeMillis();
+        System.out.println(myWord + " generated\n" +
+                attempts + " attempts\n" +
+                "Time spent: " + (timeAfter - timeBefore) + " ms");
+        executor.shutdown();
+    }
+}
---
+++ src/com/mennomuller/WordGenerator.java
@@ -0,0 +1,34 @@
+package com.mennomuller;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class WordGenerator implements Callable<Integer> {
+    private final String target;
+    private int attempts;
+    private final Random random = new Random();
+
+    public WordGenerator(String string) {
+        this.target = string;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        attempts = 0;
+        String word;
+        do{
+            word = generateWord();
+        } while (!word.equals(target));
+        return attempts;
+    }
+
+    public String generateWord(){
+        String word = "";
+        for(int i=0;i<target.length();i++){
+            word += (char)('a' + random.nextInt(26));
+        }
+        attempts++;
+        return word;
+    }
+
+}
